a5b04c1aab645c939bbb282f14a2a1c54824ec75
larry <22062019@qq.com>
2018-04-01 19:51:20 +0800

    nice work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Nice work!
                 </div>
 
                 <div class="links">
