8c64aa9a2c9a18bdc39e38936f61ee779e101ad9
Larry <22062019@qq.com>
2018-04-05 10:45:56 +0000

    updated

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>@yield('title', 'Sample2 App') - 第二遍</title>
     <link rel="stylesheet" href="/css/app.css">
   </head>
@@ -9,6 +9,7 @@
     @include('layouts._header')
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
         </div>
