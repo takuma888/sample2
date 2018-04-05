aa84dc163b24b08b0411aacd3ea1a08e55df5274
Larry <22062019@qq.com>
2018-04-05 05:03:26 +0000

    updated

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> -->
+    <title>@yield('title', 'Sample2 App') - 第二遍</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+        </div>
+    </div>
+  </body>
+</html>
